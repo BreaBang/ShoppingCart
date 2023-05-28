cc32eb0ce9f43192588d92bc0389dd59f4aa078d
breabang <breanna.bang@gmail.com>
2023-05-28 16:12:06 -0600

    updated cart function

--- main.js
+++ main.js
@@ -30,25 +30,29 @@ let shopItemsData = [{
     img: "/images/img.4.jpg"
 }]
 
+let basket = []
+
 let generateShop = () => {
-    return (shop.innerHTML = shopItemsData.map((x)=>{
+    return (shop.innerHTML = shopItemsData
+        .map((x)=>{
+            let {id, name, price, desc, img} = x;
         return `
-        <div class="item">
-        <img src="${x.img}" class="shop-image">
+        <div class="item" id=product-id-${id}>
+        <img src=${img} class="shop-image">
         <div class="details">
-            <h3>${x.name}</h3>
-            <p>${x.desc}</p>
+            <h3>${name}</h3>
+            <p>${desc}</p>
             <div class="price-quantity">
-                <h2>${x.price}</h2>
+                <h2>${price}</h2>
                 <div class="buttons">
                     <!-- Minus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16" onclick="decrease(${id})" >
                         <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
                       </svg>
                     <!-- Quantity -->
-                    <div class="quantity">0</div>
+                    <div id= ${id} class="quantity">0</div>
                     <!-- Plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16" onclick="increase(${id})">
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                       </svg>
                 </div>
@@ -59,4 +63,46 @@ let generateShop = () => {
     }).join(""));
 };
 
-generateShop()
+generateShop();
+
+let increase = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search===undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        })
+    } else{
+        search.item += 1;
+    }
+
+   
+
+    console.log(basket)
+    update(selectedItem.id)
+}
+let decrease = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search.item === 0) return;
+     else{
+        search.item -= 1;
+    }
+
+    console.log(basket)
+    update(selectedItem.id)
+}
+let update = (id) => {
+    let search = basket.find((x) => x.id === id );
+    document.getElementById(id).innerHTML = search.item
+    calculation()
+};
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartItemsAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y)=> x+y, 0);
+    
+};
