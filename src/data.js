2e777a7a5c5bff1913c004734c33660dfe317b1d
breabang <breanna.bang@gmail.com>
2023-06-04 10:33:45 -0600

    update

--- src/data.js
+++ src/data.js
@@ -3,26 +3,26 @@ let shopItemsData = [{
     name: "Sound Bath",
     price: "15",
     desc: "Relax and recharge. Online group class. 45-minutes.",
-    img: "/images/img.1.jpg"
+    img: "images/img.1.jpg"
 }, 
 {
     id:"O2",
     name: "Private Class",
     price: "42",
     desc: "Online 45-minuges session tailored to your needs.",
-    img: "/images/img.2.jpg"
+    img: "images/img.2.jpg"
 }, 
 {
     id:"O3",
     name: "Breath Workshop",
     price: "20",
     desc: "Online group workshop covering breath work basics.",
-    img: "/images/img.3.jpg"
+    img: "images/img.3.jpg"
 }, 
 {
     id:"O4",
     name: "Yoga Class",
     price: "15",
     desc: "Find balance and restoration. Online group class. 45-minutes.",
-    img: "/images/img.4.jpg"
+    img: "images/img.4.jpg"
 }]
